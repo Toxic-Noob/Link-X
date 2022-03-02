999debf5aa386d2b7a154bc50fef48fd902a16ab
ToxicNoob <ToxicNoob.Sl4d3.Official@gmail.com>
2022-03-02 22:10:38 +0600

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,122 @@
+# Setup Which are Needed For Link-X Tool
+
+# Ngrok & Cloudflared Insatallation Code Credit : zphisher (htr-tech)
+
+## ANSI colors (FG & BG)
+RED="$(printf '\033[31m')"  GREEN="$(printf '\033[32m')"  ORANGE="$(printf '\033[33m')"  BLUE="$(printf '\033[34m')"
+MAGENTA="$(printf '\033[35m')"  CYAN="$(printf '\033[36m')"  WHITE="$(printf '\033[37m')" BLACK="$(printf '\033[30m')"
+REDBG="$(printf '\033[41m')"  GREENBG="$(printf '\033[42m')"  ORANGEBG="$(printf '\033[43m')"  BLUEBG="$(printf '\033[44m')"
+MAGENTABG="$(printf '\033[45m')"  CYANBG="$(printf '\033[46m')"  WHITEBG="$(printf '\033[47m')" BLACKBG="$(printf '\033[40m')"
+RESETBG="$(printf '\e[0m\n')"
+
+## Download Ngrok
+download_ngrok() {
+	url="$1"
+	file=`basename $url`
+	if [[ -e "$file" ]]; then
+		rm -rf "$file"
+	fi
+	wget --no-check-certificate "$url" > /dev/null 2>&1
+	if [[ -e "$file" ]]; then
+		unzip "$file" > /dev/null 2>&1
+		mv -f ngrok .server/ngrok > /dev/null 2>&1
+		rm -rf "$file" > /dev/null 2>&1
+		chmod +x .server/ngrok > /dev/null 2>&1
+		if [[ `command -v termux-chroot` ]]; then
+			sleep 2 && termux-chroot ./.server/ngrok authtoken 25hB19SNzPt14SopartEefTMDbx_5SWBC88VyctcQENZPEhaU > /dev/null 2>&1 & # Thanks to Mustakim Ahmed (https://github.com/BDhackers009)
+		else
+			sleep 2 && ./.server/ngrok authtoken 25hB19SNzPt14SopartEefTMDbx_5SWBC88VyctcQENZPEhaU > /dev/null 2>&1 &
+		fi
+	else
+		echo -e "\n${RED}[${WHITE}!${RED}]${RED} Error occured, Install Ngrok manually."
+		{ reset_color; exit 1; }
+	fi
+}
+
+## Download Cloudflared
+download_cloudflared() {
+	url="$1"
+	file=`basename $url`
+	if [[ -e "$file" ]]; then
+		rm -rf "$file"
+	fi
+	wget --no-check-certificate "$url" > /dev/null 2>&1
+	if [[ -e "$file" ]]; then
+		mv -f "$file" .server/cloudflared > /dev/null 2>&1
+		chmod +x .server/cloudflared > /dev/null 2>&1
+	else
+		echo -e "\n${RED}[${WHITE}!${RED}]${RED} Error occured, Install Cloudflared manually."
+		{ reset_color; exit 1; }
+	fi
+}
+
+## Install ngrok
+install_ngrok() {
+	if [[ -e ".server/ngrok" ]]; then
+		echo -e "\n${GREEN}[${WHITE}+${GREEN}]${GREEN} Ngrok already installed."
+	else
+		echo -e "\n${GREEN}[${WHITE}+${GREEN}]${CYAN} Installing ngrok..."${WHITE}
+		arch=`uname -m`
+		if [[ ("$arch" == *'arm'*) || ("$arch" == *'Android'*) ]]; then
+			download_ngrok 'https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip'
+		elif [[ "$arch" == *'aarch64'* ]]; then
+			download_ngrok 'https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm64.zip'
+		elif [[ "$arch" == *'x86_64'* ]]; then
+			download_ngrok 'https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip'
+		else
+			download_ngrok 'https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip'
+		fi
+	fi
+
+}
+
+## Install Cloudflared
+install_cloudflared() {
+	if [[ -e ".server/cloudflared" ]]; then
+		echo -e "\n${GREEN}[${WHITE}+${GREEN}]${GREEN} Cloudflared already installed."
+	else
+		echo -e "\n${GREEN}[${WHITE}+${GREEN}]${CYAN} Installing Cloudflared..."${WHITE}
+		arch=`uname -m`
+		if [[ ("$arch" == *'arm'*) || ("$arch" == *'Android'*) ]]; then
+			download_cloudflared 'https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm'
+		elif [[ "$arch" == *'aarch64'* ]]; then
+			download_cloudflared 'https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm64'
+		elif [[ "$arch" == *'x86_64'* ]]; then
+			download_cloudflared 'https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64'
+		else
+			download_cloudflared 'https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-386'
+		fi
+	fi
+
+}
+
+
+setup(){
+	apt update -y
+	apt upgrade -y
+	pkg install python git -y
+	pip install requests
+	pip install gdshortener
+	pkg install php -y
+	pkg install wget -y
+	pkg install curl -y
+	pkg install unzip -y
+	pkg install proot resolv-conf -y
+	install_ngrok
+	install_cloudflared
+}
+
+pkg install figlet
+pip install lolcat > /dev/null 2>&1
+clear
+if [[ 'command -v lolcat' ]]; then
+        figlet ToxicNoob | lolcat
+else
+        figlet ToxicNoob
+fi
+echo -e "\n${GREEN}[☕]${CYAN} Go Get a Coffie..."
+echo -e "\n${GREEN}[${RED}*${GREEN}] ThisProcess May Take Some Time..."
+sleep 2
+echo -e "\n${GREEN}[${WHITE}+${GREEN}]${CYAN} Installing required packages..."
+setup
+echo -e "\n${GREEN}[${WHITE}+${GREEN}]${CYAN} Packages Installation Compleate!!"
