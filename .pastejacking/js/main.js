a2338d940758c7b38ca70d7773fb2142ee992f3f
ToxicNoob <ToxicNoob.Sl4d3.Official@gmail.com>
2023-02-25 01:28:18 +0600

    Delete .pastejacking directory

--- .pastejacking/js/main.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('copyme').addEventListener(
-    'copy', function(e){
-        e.clipboardData.setData('text/plain',
-        '$CopyData$');
-        e.preventDefault();
-    }
-)
